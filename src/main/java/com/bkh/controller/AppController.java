fe0b8f5a0543196c0cd97ab7e05677a0d4b2fde2
Biraj Kumar Halder <birajkumarhalder@gmail.com>
2022-03-10 20:40:56 +0530

    service added

--- src/main/java/com/bkh/controller/AppController.java
+++ src/main/java/com/bkh/controller/AppController.java
@@ -1,15 +1,21 @@
 package com.bkh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bkh.service.AppService;
+
 @RestController
 public class AppController {
 
+	@Autowired
+	private AppService appService;
+
 	@GetMapping("/")
-	public ResponseEntity<String> readExcel() {
-		return ResponseEntity.ok("Hello World");
+	public ResponseEntity<String> sayHello() {
+		return ResponseEntity.ok(appService.sayHello());
 	}
 
 }
---
+++ src/main/java/com/bkh/service/AppService.java
@@ -0,0 +1,12 @@
+package com.bkh.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppService {
+
+	public String sayHello() {
+		return "Hello world!!";
+	}
+
+}
